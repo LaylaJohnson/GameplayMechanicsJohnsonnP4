73c89cac9c145325d919deade2b658bcfd7447dd
Layla <756830@student.dusd.net>
2023-09-11 11:31:04 -0700

    updated the game

--- GameplayMechanicsJohnsonP4/Assets/Scripts/SpawnManager.cs
+++ GameplayMechanicsJohnsonP4/Assets/Scripts/SpawnManager.cs
@@ -5,24 +5,33 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab;
+    public GameObject powerupPrefab;
     private float spawnRange = 9.0f;
     public int enemyCount;
+    public int waveNumber = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        SpawnEnemyWave(3);
+        SpawnEnemyWave(waveNumber);
     }
 
     // Update is called once per frame
     void Update()
     {
-        enemyCount = FindObjectOfType<Enemy>().Length;
+        enemyCount = FindObjectsOfType<Enemy>().Length;
+
+        if(enemyCount == 0)
+        {
+            waveNumber++;
+            SpawnEnemyWave(waveNumber);
+            Instantiate(powerupPrefab, GenerateSpawnPosition(), powerupPrefab.transform.rotation);
+        }
     }
 
-    void SpawnEnemyWave(int enemiesToSpawnn)
+    void SpawnEnemyWave(int enemiesToSpawn)
     {
-        for (int i = 0; i < enemiesToSpawnn; i++)
+        for (int i = 0; i < enemiesToSpawn; i++)
 
             Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
     }
